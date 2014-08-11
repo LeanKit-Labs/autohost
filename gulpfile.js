aa62af8cdbe916455af7a1dc6328b2b2e9003a59
Alex Robson <asrobson@gmail.com>
2014-08-11 16:45:12 -0500

    Work around peculiar gulp-mocha "features" :speak_no_evil:

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,20 @@ var gulp = require( 'gulp' ),
 	mocha = require( 'gulp-mocha' );
 
 gulp.task( 'test', function() {
-	gulp.src( [ './spec/websocket/*.spec.js', './spec/socketio/*.spec.js', './spec/**.spec.js' ] )
+	return gulp.src( [ './spec/websocket/*.spec.js', './spec/socketio/*.spec.js', './spec/**.spec.js' ], { read: false } )
 		.pipe( mocha( { reporter: 'spec' } ) )
-		.on( 'error', function( err ) { console.log( err.message ); } );
+		.on( 'end', process.exit.bind( process, 0 ) )
+		.on( 'error', process.exit.bind( process, 1 ) );
+} );
+
+gulp.task( 'continuous-test', function() {
+	return gulp.src( [ './spec/websocket/*.spec.js', './spec/socketio/*.spec.js', './spec/**.spec.js' ], { read: false } )
+		.pipe( mocha( { reporter: 'spec' } ) );
 } );
 
 gulp.task( 'watch', function() {
-	gulp.watch( [ './src/**', './spec/**' ], [ 'test' ] );
+	gulp.watch( [ './src/**', './spec/**' ], [ 'continuous-test' ] );
 } );
 
-gulp.task( 'default', [ 'test', 'watch' ], function() {
+gulp.task( 'default', [ 'continuous-test', 'watch' ], function() {
 } );
