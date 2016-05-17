e5387483378e820538e0017c3f13ef963515418b
EvanSnapp <evan.snapp@leankit.com>
2016-05-17 11:28:44 -0500

    Remove Express.js warning when not using sessions

--- src/log.js
+++ src/log.js
@@ -2,7 +2,7 @@ var _ = require( 'lodash' );
 var logFn = require( 'whistlepunk' ).log;
 logFn( {} );
 module.exports = function( config, ns ) {
-	if( process.env.DEBUG ) {
+	if ( process.env.DEBUG ) {
 		ns = _.isString( config ) ? config : ns;
 		return ns ? logFn( ns ) : logFn;
 	} else {
