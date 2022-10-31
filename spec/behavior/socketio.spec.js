c4e4f48b22959894137780fd6580ba23c4a4d60c
Ryan Niemeyer <rniemeyer@gmail.com>
2022-10-31 11:31:36 -0500

    Updated socket.io

--- spec/behavior/socketio.spec.js
+++ spec/behavior/socketio.spec.js
@@ -1,10 +1,10 @@
-var path = require( 'path' );
-var proxyquire = require( 'proxyquire' ).noPreserveCache();
+var path = require('path');
+var proxyquire = require('proxyquire').noPreserveCache();
 
-describe( 'SocketIO', function() {
-	describe( 'with SocketIO Configuration', function() {
+describe('SocketIO', function () {
+	describe('with SocketIO Configuration', function () {
 		var instance, moduleStubs, ioStub, httpStub, registryStub;
-		before( function() {
+		before(function () {
 			ioStub = {
 				on: sinon.stub(),
 				removeAllListeners: sinon.stub(),
@@ -12,13 +12,13 @@ describe( 'SocketIO', function() {
 				engine: {}
 			};
 
-			ioFactory = sinon.stub().returns( ioStub );
+			ioFactory = sinon.stub().returns(ioStub);
 
 			httpStub = {
 				server: 'someserver',
-				getAuthMiddleware: sinon.stub().returns( {
+				getAuthMiddleware: sinon.stub().returns({
 					use: sinon.stub()
-				} )
+				})
 			};
 
 			registryStub = {
@@ -31,29 +31,30 @@ describe( 'SocketIO', function() {
 			moduleStubs = {
 				'socket.io': ioFactory
 			};
-			instanceFactory = proxyquire( path.resolve( __dirname, '../../src/websocket/socketio.js' ), moduleStubs );
-			instance = instanceFactory( {
+			instanceFactory = proxyquire(path.resolve(__dirname, '../../src/websocket/socketio.js'), moduleStubs);
+			instance = instanceFactory({
 				socketio: {
 					pingTimeout: 123456789,
 					pingInterval: 987654321,
-					transports: [ 'polling', 'websocket' ]
+					transports: ['polling', 'websocket']
 				}
-			}, registryStub );
-			instance.configure( httpStub );
-		} );
+			}, registryStub);
+			instance.configure(httpStub);
+		});
 
-		it( 'should pass the socketio config into socketio module', function() {
-			ioFactory.should.be.calledWith( 'someserver', {
+		it('should pass the socketio config into socketio module', function () {
+			ioFactory.should.be.calledWith('someserver', {
 				pingTimeout: 123456789,
 				pingInterval: 987654321,
-				transports: [ 'polling', 'websocket' ],
-				destroyUpgrade: false
-			} );
-		} );
+				transports: ['polling', 'websocket'],
+				destroyUpgrade: false,
+				allowRequest: sinon.match.func
+			});
+		});
 
-		describe( 'when establishing a new connection', function() {
+		describe('when establishing a new connection', function () {
 			var socketStub;
-			before( function() {
+			before(function () {
 				socketStub = {
 					request: {
 						headers: {
@@ -67,67 +68,67 @@ describe( 'SocketIO', function() {
 					close: sinon.stub(),
 					on: sinon.stub()
 				};
-				ioStub.on.callArgWith( 1, socketStub );
-			} );
+				ioStub.on.callArgWith(1, socketStub);
+			});
 
-			it( 'should set the socket type', function() {
-				socketStub.type.should.equal( 'socketio' );
-			} );
+			it('should set the socket type', function () {
+				socketStub.type.should.equal('socketio');
+			});
 
-			it( 'should set a user if request lacks one', function() {
-				socketStub.user.should.eql( { id: 'anonymous', name: 'anonymous' } );
-			} );
+			it('should set a user if request lacks one', function () {
+				socketStub.user.should.eql({ id: 'anonymous', name: 'anonymous' });
+			});
 
-			it( 'should copy the session to the socket', function() {
-				socketStub.session.should.equal( socketStub.request.session );
-			} );
+			it('should copy the session to the socket', function () {
+				socketStub.session.should.equal(socketStub.request.session);
+			});
 
-			it( 'should parse cookie(s) into socket cookies property', function() {
-				socketStub.cookies.should.eql( {
+			it('should parse cookie(s) into socket cookies property', function () {
+				socketStub.cookies.should.eql({
 					yumyum: 'nomnom',
 					chocchip: 'chewy'
-				} );
-			} );
+				});
+			});
 
-			describe( 'when the client identifies itself', function() {
+			describe('when the client identifies itself', function () {
 				var fakeIdData;
-				before( function() {
+				before(function () {
 					fakeIdData = {
 						id: 'oh, hai'
 					};
-					socketStub.on.callArgWith( 1, fakeIdData );
-				} );
+					socketStub.on.callArgWith(1, fakeIdData);
+				});
 
-				it( 'should set the socket id', function() {
-					socketStub.id.should.equal( 'oh, hai' );
-				} );
+				it('should set the socket id', function () {
+					socketStub.id.should.equal('oh, hai');
+				});
 
-				it( 'should add socket to the registry', function() {
-					registryStub.add.should.be.calledWith( socketStub );
-				} );
-			} );
-		} );
+				it('should add socket to the registry', function () {
+					registryStub.add.should.be.calledWith(socketStub);
+				});
+			});
+		});
 
-		describe( 'when registering a handler for client events', function() {
+		describe('when registering a handler for client events', function () {
 			var fakeClient, cbStub;
-			before( function() {
+			before(function () {
 				fakeClient = {
 					type: 'socketio',
 					on: sinon.stub()
 				};
-				registryStub.clients = [ fakeClient ];
+				registryStub.clients = [fakeClient];
 				cbStub = sinon.stub();
-				instance.on( 'a.topic', cbStub );
-			} );
+				instance.on('a.topic', cbStub);
+			});
 
-			it( 'should register a callback with the client', function() {
-				fakeClient.on.should.be.calledWithMatch( 'a.topic', sinon.match.func );
-			} );
+			it('should register a callback with the client', function () {
+				fakeClient.on.should.be.calledWithMatch('a.topic', sinon.match.func);
+			});
 
-			it( 'should invoke registed callback on client events', function() {
-				fakeClient.on.callArgWith( 1, { foo: 'bar' } );
-				cbStub.should.be.calledWithMatch( { foo: 'bar' }, fakeClient );
-			} );
-		} );
-	} );
-} );
+			it('should invoke registed callback on client events', function () {
+				fakeClient.on.callArgWith(1, { foo: 'bar' });
+				cbStub.should.be.calledWithMatch({ foo: 'bar' }, fakeClient);
+			});
+		});
+	});
+});
--- spec/setup.js
+++ spec/setup.js
@@ -1,43 +1,43 @@
-var chai = require( 'chai' );
-chai.use( require( 'chai-as-promised' ) );
+var chai = require('chai');
+chai.use(require('chai-as-promised'));
 global.should = chai.should();
 global.expect = chai.expect;
-var _ = global._ = require( 'lodash' );
-global.when = require( 'when' );
-global.lift = require( 'when/node' ).lift;
-global.seq = require( 'when/sequence' );
-global.fs = require( 'fs' );
-global.path = require( 'path' );
-global.sinon = require( 'sinon' );
-global.proxyquire = require( 'proxyquire' ).noPreserveCache();
-var sinonChai = require( 'sinon-chai' );
-chai.use( sinonChai );
-var logLib = require( "../src/log" );
-var adapterPath = require.resolve( "./mockLogger.js" );
-var mockAdapter = require( "./mockLogger.js" );
+var _ = global._ = require('lodash');
+global.when = require('when');
+global.lift = require('when/node').lift;
+global.seq = require('when/sequence');
+global.fs = require('fs');
+global.path = require('path');
+global.sinon = require('sinon');
+global.proxyquire = require('proxyquire').noPreserveCache();
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+var logLib = require("../src/log");
+var adapterPath = require.resolve("./mockLogger.js");
+var mockAdapter = require("./mockLogger.js");
 process.title = 'ahspec';
 
 function transformResponse() {
-	var props = Array.prototype.slice.call( arguments, 0 );
-	return function( resp ) {
-		resp = _.isArray( resp ) ? resp[ 0 ] : resp;
+	var props = Array.prototype.slice.call(arguments, 0);
+	return function (resp) {
+		resp = _.isArray(resp) ? resp[0] : resp;
 		var obj = {};
-		if ( _.includes( props, 'body' ) ) {
+		if (_.includes(props, 'body')) {
 			obj.body = resp.body;
 		}
-		if ( _.includes( props, 'type' ) ) {
-			obj.type = resp.headers[ 'content-type' ];
+		if (_.includes(props, 'type')) {
+			obj.type = resp.headers['content-type'];
 		}
-		if ( _.includes( props, 'cache' ) ) {
-			obj.cache = resp.headers[ 'cache-control' ];
+		if (_.includes(props, 'cache')) {
+			obj.cache = resp.headers['cache-control'];
 		}
-		if ( _.includes( props, 'testHeader' ) ) {
-			obj.header = resp.headers[ 'test-header' ];
+		if (_.includes(props, 'testHeader')) {
+			obj.header = resp.headers['test-header'];
 		}
-		if ( _.includes( props, 'setCookie' ) ) {
-			obj.cookie = resp.headers[ 'set-cookie' ][ 0 ];
+		if (_.includes(props, 'setCookie')) {
+			obj.cookie = resp.headers['set-cookie'][0];
 		}
-		if ( _.includes( props, 'statusCode' ) ) {
+		if (_.includes(props, 'statusCode')) {
 			obj.statusCode = resp.statusCode;
 		}
 		return obj;
@@ -48,24 +48,24 @@ function onError() {
 	return {};
 }
 
-global.setupLog = function setupLog( ns, level ) {
-	global.logAdapter = mockAdapter( ns );
+global.setupLog = function setupLog(ns, level) {
+	global.logAdapter = mockAdapter(ns);
 	var adapters = {};
-	adapters[ adapterPath ] = { level: level || 5 };
-	var logFn = global.Log = logLib( {
+	adapters[adapterPath] = { level: level || 5 };
+	var logFn = global.Log = logLib({
 		adapters: adapters
-	} );
-	return logFn( ns );
+	});
+	return logFn(ns);
 };
 
 global.transformResponse = transformResponse;
 global.onError = onError;
 
 var _log = console.log;
-console.log = function() {
-	if ( typeof arguments[ 0 ] === 'string' && /^[a-zA-Z]/.test( arguments[ 0 ] ) ) {
+console.log = function () {
+	if (typeof arguments[0] === 'string' && /^[a-zA-Z]/.test(arguments[0])) {
 		return; // swallow this message
 	} else {
-		_log.apply( console, arguments );
+		_log.apply(console, arguments);
 	}
 };
